cdc790bbcf4cf9d3669f6abfdae39b746234621f
Lance Gleason <lgleasain@yahoo.com>
2015-11-30 12:02:40 -0500

    binding the service

--- app/src/main/java/com/polyglotprogramminginc/andevcon/MainActivity.java
+++ app/src/main/java/com/polyglotprogramminginc/andevcon/MainActivity.java
@@ -1,15 +1,24 @@
 package com.polyglotprogramminginc.andevcon;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.mbientlab.metawear.MetaWearBleService;
+
+public class MainActivity extends AppCompatActivity implements ServiceConnection{
+    private MetaWearBleService.LocalBinder mwBinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getApplicationContext().bindService(new Intent(this, MetaWearBleService.class),
+                this, Context.BIND_AUTO_CREATE);
     }
 
     @Override
@@ -40,5 +51,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        ///< Get a reference to the MetaWear service from the binder
+        mwBinder = (MetaWearBleService.LocalBinder) service;
+        Log.i("Main Activity", "Service Connected");
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        Log.i("Main Activity", "Service Disconnected");
     }
 }
